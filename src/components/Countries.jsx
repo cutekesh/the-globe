641dd16618c728d764820572214d40b78d6d5ccd
CK <cutekesh.21@gmail.com>
2025-01-14 11:24:03 -0800

    slice

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -14,7 +14,7 @@ const Countries = ({ allCountries }) => {
             <div className="shadow rounded-2 custom-text-color bg-elements">
               <img className="rounded-top-2" src={country.flags.png} alt="" />
               <div className="text-start p-4">
-                <h3 className="mb-3">{country.name.common}</h3>
+                <h3 className="mb-3">{country.name.common.slice(0, 15)}</h3>
                 <p className="mb-1">
                   <b>Population:</b> {country.population.toLocaleString()}
                 </p>
@@ -22,7 +22,7 @@ const Countries = ({ allCountries }) => {
                   <b>Region:</b> {country.region}
                 </p>
                 <p className="mb-1">
-                  <b>Capital:</b> {country.capital}
+                  <b>Capital:</b> {country?.capital?.slice(0, 15)}
                 </p>
               </div>
             </div>
